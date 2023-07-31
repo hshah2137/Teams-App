184d912e2b153c6a41f93cdfa844a7ea1d3cda8e
hshah2137 <ucabahx@ucl.ac.uk>
2023-07-31 11:45:31 +0100

    routes

--- client/react/src/App.tsx
+++ client/react/src/App.tsx
@@ -6,73 +6,24 @@ import {
 } from '@azure/communication-react';
 import React, { useState, useMemo, useEffect } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route }from 'react-router-dom';
+import AutoCall from './AutoCall';
+import InputPage from './input';
 
 const App = () => { 
-  const displayName = 'Guest'
-  const [userId, setUserId] = useState<string>('');
-  const [token, setToken] = useState<string>('');
-  const [teamsMeetingLink, setTeamsMeetingLink] = useState<string>('');
-  const [message, setMessage] = useState<string>('');
-  const credential = useMemo(() => {
-    if (token) {
-      return new AzureCommunicationTokenCredential(token)
-    }
-    return;
-    }, [token]);
+ 
+  return(
+    <Router>
 
-  const callAdapterArgs = useMemo(() => {
-    if (userId && credential && displayName && teamsMeetingLink) {
-      return {
-        userId: fromFlatCommunicationIdentifier(userId) as CommunicationUserIdentifier,
-        displayName,
-        credential,
-        locator: { meetingLink: teamsMeetingLink },
-      }
-    }
-    return {};
-  }, [userId, credential, displayName, teamsMeetingLink]);
+      <Routes>
 
-  const callAdapter = useAzureCommunicationCallAdapter(callAdapterArgs);
+        <Route path = '/' element = {<AutoCall/>} />
+        <Route path = '/input' element = {<InputPage/>} />
 
-  useEffect(() => {
-    const init = async () => {
-      setMessage('Getting ACS user');
-      //Call Azure Function to get the ACS user identity and token
-      const res = await fetch(process.env.REACT_APP_ACS_USER_FUNCTION as string);
-      const user = await res.json();
-      setUserId(user.userId);
-      setToken(user.token);
+      </Routes>
 
-      setMessage('Getting Teams meeting link...');
-      //Call Azure Function to get the meeting link
-      const resTeams = await fetch(process.env.REACT_APP_TEAMS_MEETING_FUNCTION as string);
-      const link = await resTeams.text();
-      setTeamsMeetingLink(link);
-      setMessage('');
-      console.log('Teams meeting link', link);
-    }
-    init();
-  }, []);
-
-  if (callAdapter) {
-    return (
-      <div>
-        <h1>Contact Customer Service</h1>
-        <div className="wrapper">
-          <CallComposite
-            adapter={callAdapter}
-          />
-        </div>
-      </div>
-    );
-  }
-  if (!credential) {
-    return <>Failed to construct credential. Provided token is malformed.</>;
-  }
-  if (message) {
-    return <div>{message}</div>;
-  }
-  return <div>Initializing...</div>;
-};
-
+    </Router>
+    
+ );
+}
 export default App;
---
+++ client/react/src/input.tsx
@@ -0,0 +1,79 @@
+import {useEffect, useRef, useState, ChangeEvent} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const InputPage = () => {
+    const [link, setLink] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleInputChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        if (event.target.value != null) {
+            setLink(event.target.value);   
+        }
+        
+        
+    };
+
+    const clickHandle = (event: React.MouseEvent<HTMLButtonElement>) =>{
+        event.preventDefault()
+        if (link === ''){
+            console.log('cannot proceed')
+
+        }
+        else{
+            console.log('good')
+            console.log(link)
+            //navigate('/joinCall', {state: {link}})
+        }
+    }
+
+    const [opacity, setOpacity] = useState(1);
+
+    const buttonStyle = {
+        backgroundColor: '#464EB8',
+        color: 'white',
+        padding: '10px 20px',
+        borderRadius: '5px',
+        border: 'none',
+        cursor: 'pointer',
+        fontSize: '16px',
+        opacity: opacity,
+      };
+
+      const handleMouseEnter = () => {
+        setOpacity(0.7);
+      };
+    
+      const handleMouseLeave = () => {
+        setOpacity(1);
+      };
+    
+ //https://teams.microsoft.com/l/meetup-join/19%3ameeting_YWNlZWEzMTMtMDM4Yy00ZjFjLThiOWUtOTQ4ZWYyMGRjODZi%40thread.v2/0?context=%7b%22Tid%22%3a%221faf88fe-a998-4c5b-93c9-210a11d9a5c2%22%2c%22Oid%22%3a%229a222fae-66e1-4dbb-845c-d10f672ec8ac%22%7d
+    return(
+        <div>
+            <h1 style = {{display: 'flex',justifyContent:'center', alignItems: 'center', marginTop: "2vh"}}>Please enter the teams meeting link</h1>
+            <form>
+                
+                <div style = {{display: 'flex',justifyContent:'center', alignItems: 'center', marginTop: "10vh"}}>
+                
+                <textarea value={link} onChange={handleInputChange} style = {{marginLeft:"2.5vw", width: "75vw", resize: 'none'}} rows={5} />
+                
+                </div>
+                <div style = {{display: 'flex',justifyContent:'center', alignItems: 'center', marginTop: "2.5vh"}}>
+                <button 
+                style = {buttonStyle} 
+                onClick={(event) => clickHandle(event)}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}>
+                    Confirm
+                </button>
+                </div>
+            </form>
+      </div>   
+    );
+
+};
+
+export default InputPage;
