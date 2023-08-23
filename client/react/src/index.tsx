72599f9f8174f8b12f6b54627b003a7241fe5844
hshah2137 <ucabahx@ucl.ac.uk>
2023-08-23 21:08:03 +0100

    tidying up

--- client/react/src/index.tsx
+++ client/react/src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App'; 
 import { initializeIcons } from '@fluentui/react/lib/Icons';
 import './index.css';
-//import ColorSchemesExample from './navbar';
-
-       
 
 ReactDOM.render(
   <div className="wrapper">
